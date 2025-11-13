44e8d39fa85f182c67d084abae2270a07a36c434
unknown <varun1492006@gmail.com>
2025-11-13 15:52:17 +0530

    Privy added to frontend

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,10 +2,12 @@
 
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { PrivyProvider } from "@privy-io/react-auth";
 
 import { QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import { queryClient } from "@/utils/trpc";
+import { baseSepolia } from "viem/chains";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,9 +29,24 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
+        <PrivyProvider
+          appId="cmhx7ul0601bpjs0cplcpxyr6"
+          clientId="client-WY6SXRcUUam8wK1NvSoKeDKWPREsvHAoKPzjdw1DEafRg"
+          config={{
+            // Create embedded wallets for users who don't have a wallet
+            embeddedWallets: {
+              ethereum: {
+                createOnLogin: "users-without-wallets",
+              },
+            },
+            defaultChain: baseSepolia,
+            supportedChains: [baseSepolia],
+          }}
+        >
+          <QueryClientProvider client={queryClient}>
+            {children}
+          </QueryClientProvider>
+        </PrivyProvider>
       </body>
     </html>
   );
